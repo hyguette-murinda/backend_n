e7dbf68b1bd678191f1a47cad6eaf83d86b13694
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-01-26 07:57:25 +0200

    project setup

---
+++ src/main/java/rca/ne/prep/NationalprepApplication.java
@@ -0,0 +1,13 @@
+package rca.ne.prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalprepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalprepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rca/ne/prep/NationalprepApplicationTests.java
@@ -0,0 +1,13 @@
+package rca.ne.prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalprepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
