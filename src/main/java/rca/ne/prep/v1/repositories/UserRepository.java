df6da453b25323d1207f0e3734f1bdb9f2692059
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-02-02 09:36:21 +0200

    user services implementation

---
+++ src/main/java/rca/ne/prep/v1/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package rca.ne.prep.v1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rca.ne.prep.v1.models.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
--- src/main/java/rca/ne/prep/v1/services/UserService.java
+++ src/main/java/rca/ne/prep/v1/services/UserService.java
@@ -1,12 +1,10 @@
-//package rca.ne.prep.v1.services;
-//
-//import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Service;
-//import rca.ne.prep.v1.models.User;
-//
-//
-//@Component
-//public interface UserService {
-//    User registerUser(User user);
-//    User findByUsername(String username);
-//}
+package rca.ne.prep.v1.services;
+
+import rca.ne.prep.v1.models.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    User registerUser(User user);
+    Optional<User> findByUsername(String username);
+}
