0b3b062e1f6a7b4201678764ec91c210f2e25e69
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-06-04 14:12:44 +0200

    updated security config file

--- src/main/java/rca/ne/prep/v1/dto/requests/CreateUserDto.java
+++ src/main/java/rca/ne/prep/v1/dto/requests/CreateUserDto.java
@@ -2,8 +2,13 @@ package rca.ne.prep.v1.dto.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class CreateUserDto {
+    @NotBlank(message = "name can't be blank")
     private String userName;
+
+    @NotBlank(message = "password can't be blank")
     private  String password;
 }
